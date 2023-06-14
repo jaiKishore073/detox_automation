578ce4d39e3770f3c7d5ec640a9e9d12bd8aa1cb
jaiKishore073 <jaikishore073@gmail.com>
2023-06-14 11:59:06 +0530

    new code

---
+++ .detoxrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  apps: {
+      'ios.debug': {
+          type: 'ios.app',
+          binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/pqaa_detox.app',
+          build: 'xcodebuild -workspace ios/pqaa_detox.xcworkspace -scheme pqaa_detox -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      },
+      'ios.release': {
+          type: 'ios.app',
+          binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/pqaa_detox.app',
+          build: 'xcodebuild -workspace ios/pqaa_detox.xcworkspace -scheme pqaa_detox -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      },
+      'android.debug': {
+          type: 'android.apk',
+          binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+          build: 'cd android && gradlew app:assembleDebug app:assembleAndroidTest -DtestBuildType=debug'
+       //build: 'cd android && ./gradlew clean && ./gradlew assembleRelease'
+      },
+      'android.release': {
+          type: 'android.apk',
+          binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+          build: 'cd android && gradlew app:assembleRelease app:assembleAndroidTest -DtestBuildType=release'
+       //build: 'cd android && ./gradlew clean && ./gradlew assembleRelease'
+      },
+
+  },
+
+  devices: {
+      simulator: {
+        type: 'ios.simulator',
+        device: {
+          type: 'iPhone 14 Pro',
+        },
+      },
+      attached: {
+        type: 'android.attached',
+        device: {
+          adbName: '.*',
+        },
+      },
+      emulator: {
+        type: 'android.emulator',
+        device: {
+          avdName: 'emulator-5554',
+        },
+      },
+    },
+
+  configurations: {
+      "ios.sim.debug": {
+          "device": "simulator",
+          "app": "ios.debug"
+      },
+      "ios.sim.release": {
+          "device": "simulator",
+          "app": "ios.release"
+      },
+      "android.emu.debug": {
+          "device": "emulator",
+          "app": "android.debug"
+      },
+      "android.emu.release": {
+          "device": "emulator",
+          "app": "android.release"
+      }
+      
+  }
+}
